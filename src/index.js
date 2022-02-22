9847089819d5e42d9fc86a9ebb4434458e17cb61
Stepan Bryl <bryl.step@tut.by>
2022-02-22 08:54:28 +0300

    feat:index.js

--- src/index.js
+++ src/index.js
@@ -38,31 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    let result = [];
-    expr = expr.replace(/\s/g, '');
-    const matrixLeter = expr.split('');
-    
-    for(let i=0; i < matrixLeter.length; i++)
-    for (const [key, value] of Object.entries(MORSE_TABLE)){
-    if(value === matrixLeter[i])
-      result.push((`${key}`));
-    }
-    const res = [];
-    res.push(result.map(function(el) {
-    return el.replace(/./g, v => /-/i.test(v)?11:10)
-    }))
-    
-    
-    function add(ar){ 
-    const arr = [];
-    for(let y = 0; y < ar.length; y++)
-     arr.push('0'.repeat(10 - ar[y].length)+ar[y]); 
-    return arr;
-    }
-    let teg = [];
-    teg.push(res.map(el => add(el)))
-      
-    return teg.join('').replace(/,/g, '');  // write your solution here
+   let arr = [];
+  let result = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    arr.push(expr.slice(i, i + 10));
+  }
+console.log(arr)
+const ar = arr.map(el => el.replace(/00/g, ''))
+console.log(ar)
+const res = ar.map(el => el.replace(/10/g, '.'))
+const resus = res.map(el => el.replace(/11/g, '-'))
+const resu = resus.map(el => el.replace('**********', ' '))
+console.log(resu)
+
+for(let i = 0; i < resu.length; i++)
+for (const [key, value] of Object.entries(MORSE_TABLE)) {
+if(resu[i]=== key)
+ result.push(`${value}`);
+}
+const good = result.join('');
+return good;  // write your solution here
 }
 
 module.exports = {
